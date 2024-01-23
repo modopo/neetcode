8cf1a6c8a18c7ff999ca924fcb6a56df06081679
Jonathan Tsai <modopo@gmail.com>
2024-01-22 17:40:01 -0800

    koko eating bananas

---
+++ binary_search/minEatingSpeed.js
@@ -0,0 +1,54 @@
+// Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+// Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+// Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+// Return the minimum integer k such that she can eat all the bananas within h hours.
+
+ 
+
+// Example 1:
+
+// Input: piles = [3,6,7,11], h = 8
+// Output: 4
+// Example 2:
+
+// Input: piles = [30,11,23,4,20], h = 5
+// Output: 30
+// Example 3:
+
+// Input: piles = [30,11,23,4,20], h = 6
+// Output: 23
+
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    let left = 1, right = 1;
+
+    for (let pile of piles) {
+        right = Math.max(right, pile);
+    };
+
+    while (left < right) {
+        let middle = Math.floor((left + right) / 2);
+        let hoursSpent = 0;
+
+        for (let pile of piles) {
+            hoursSpent += Math.ceil(pile / middle);
+        }
+
+        if (hoursSpent <= h) {
+            right = middle;
+        } else {
+            left = middle + 1;
+        }
+    }
+
+    return left;
+
+
+};
